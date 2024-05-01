acf0e22653ed4af7e40718ce2d3114dc97474219
fredpatch <f.patchelli@gmail.com>
2024-05-01 18:25:30 +0100

    Post comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,4 +73,9 @@ class User extends Authenticatable
     {
         return $this->likes()->where('post_id', $post->id)->exists();
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
